609820f09567e94aa9bb21ce525b98c0adbba26a
管鐘 <t126017977@gmail.com>
2023-11-19 01:52:41 +0800

    correct some type-hinting (str -> Path)

--- services/serialize.py
+++ services/serialize.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 import json
 import pickle
 from typing import Any
+from pathlib import Path
 
 
 # 使用策略模式 (Strategy Pattern) 設計 Accessor 
@@ -10,29 +11,29 @@ from typing import Any
 
 class Serializer(ABC):
     @abstractmethod
-    def read(self, filepath: str) -> dict[str, Any]:
+    def read(self, filepath: Path) -> dict[str, Any]:
         pass
     @abstractmethod
-    def write(self, obj: dict[str, Any], filepath: str) -> None:
+    def write(self, obj: dict[str, Any], filepath: Path) -> None:
         pass
 
 
 class Json(Serializer):
-    def read(self, filepath: str) -> dict[str, Any]:
+    def read(self, filepath: Path) -> dict[str, Any]:
         with open(filepath, mode="r", encoding="utf-8") as json_file:
             obj = json.load(json_file)
         return obj
-    def write(self, obj: dict[str, Any], filepath: str) -> None:
+    def write(self, obj: dict[str, Any], filepath: Path) -> None:
         with open(filepath, mode="w", encoding="utf-8") as json_file:
             json.dump(obj, json_file)
 
 
 class Pickle(Serializer):
-    def read(self, filepath: str) -> dict[str, Any]:
+    def read(self, filepath: Path) -> dict[str, Any]:
         with open(filepath, mode="rb") as pkl_file:
             obj = pickle.load(pkl_file)
         return obj
-    def write(self, obj: dict[str, Any], filepath: str) -> None:
+    def write(self, obj: dict[str, Any], filepath: Path) -> None:
         with open(filepath, mode="wb") as pkl_file:
             pickle.dump(obj, pkl_file)
 
@@ -40,9 +41,9 @@ class Pickle(Serializer):
 class Accessor():
     def __init__(self, accessor: Serializer):
         self.accessor = accessor
-    def read(self, filepath: str):
+    def read(self, filepath: Path):
         return self.accessor.read(filepath)
-    def write(self, obj: dict[str, Any], filepath: str) -> None:
+    def write(self, obj: dict[str, Any], filepath: Path) -> None:
         return self.accessor.write(obj, filepath)
 
 
